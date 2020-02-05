83303261eb7c4ea7f5e600590f74c29e61962a28
Ivandi Santoso <ivandisantoso@Vahns-MacBook-Pro.local>
2020-02-05 01:43:32 -0500

    login+signin page created

--- server.js
+++ server.js
@@ -8,51 +8,7 @@ const settings = {
     starsPath: `img/stars/`
   }
 
-const allRooms =[
-  { //0
-    id : 101,
-    roomImg : settings.imagePath+`img01.jpg`,
-    starImg: settings.starsPath+`2stars.png`,
-    title: `Champ Super 1`,
-    description: `champion`,
-    price: 39
-  },{ //1
-    id : 102,
-    roomImg : settings.imagePath+`img02.jpg`,
-    starImg: settings.starsPath+`3stars.png`,
-    title: `Nike Super 1`,
-    description: `nike`,
-    price: 59
-  },{ //2
-    id : 103,
-    roomImg : settings.imagePath+`img03.jpg`,
-    starImg: settings.starsPath+`4stars.png`,
-    title: `Entire apartment ...`,
-    description: `Designer Loft Bangkok Silom`,
-    price: 102 
-  },{ //3
-    id : 104,
-    roomImg : settings.imagePath+`img04.jpg`,
-    starImg: settings.starsPath+`1star.png`,
-    title: `Entire Chalet. South Lake ...`,
-    description: `The Lake Tahoe Chalet`,
-    price: 49
-  },{ //4
-    id : 105,
-    roomImg : settings.imagePath+`img05.jpg`,
-    starImg: settings.starsPath+`0star.png`,
-    title: `Entire house ...`,
-    description: `The Lake Tahoe Chalet`,
-    price: 149 
-  },{ //5
-    id : 106,
-    roomImg : settings.imagePath+`img06.jpg`,
-    starImg: settings.starsPath+`5stars.png`,
-    title: `Champ Super 2`,
-    description: `champion`,
-    price: 139
-  }
-];
+
 
 //This allows express to make my static content available from the public
 app.use(express.static('public'))
@@ -60,20 +16,26 @@ app.use(express.static('public'))
 //This tells Express to set or register Handlebars as its' Template/View Engine
 app.engine('hbs', exphbs({
   layoutsDir:`${__dirname}/views/layouts`,
-  extname: 'hbs',
-  defaultLayout:'main',
-  partialsDir:`${__dirname}/views/partials`
+  partialsDir:`${__dirname}/views/partials`,
+  // defaultLayout:'main',
+  extname: 'hbs'
+
 }));
 
+// hbs.registerPartials(__dirname + '/views/partials');
+
+//set the view engine to use handlebars
 app.set('view engine', 'hbs');
+// app.set('views',__dirname + '/views');
+
 
 //set up routes
 app.get('/',(req,res)=>{
     res.render('index',{
         title: "Home",
-        headingInfo : "Home Page",
-        rooms: allRooms,
-        randomContent: "BLAH BLAH BLHA"
+        headingInfo : "Home Page"
+        
+        // randomContent: "BLAH BLAH BLHA"
     });
     
 });
@@ -83,6 +45,13 @@ app.get('/user-registration',(req,res)=>{
   res.render('userregistration',{
       title: "User Registration",
       headingInfo : "User Registration Page",
+  });
+});
+
+app.get('/login',(req,res)=>{
+  res.render('login',{
+      title: "Login"
+      // headingInfo : "User Registration Page",
   });
 });
 
@@ -113,56 +82,57 @@ app.get('/room-listing',(req,res)=>{
     // fakeDB.push({title:'XPS 17',description:`XPS 17 is designed to keep you entertained for more than 9 hours 
     // with a 9-cell battery upgrade.`,price:`1949.99`});
 
-    // const allRooms =[
-    //     { //0
-    //       id : 101,
-    //       roomImg : settings.imagePath+`img01.jpg`,
-    //       starImg: settings.starsPath+`2stars.png`,
-    //       title: `Champ Super 1`,
-    //       description: `champion`,
-    //       price: 39
-    //     },{ //1
-    //       id : 102,
-    //       roomImg : settings.imagePath+`img02.jpg`,
-    //       starImg: settings.starsPath+`3stars.png`,
-    //       title: `Nike Super 1`,
-    //       description: `nike`,
-    //       price: 59
-    //     },{ //2
-    //       id : 103,
-    //       roomImg : settings.imagePath+`img03.jpg`,
-    //       starImg: settings.starsPath+`4stars.png`,
-    //       title: `Adidas Super 1`,
-    //       description: `adidas`,
-    //       price: 69
-    //     },{ //3
-    //       id : 104,
-    //       roomImg : settings.imagePath+`img04.jpg`,
-    //       starImg: settings.starsPath+`1star.png`,
-    //       title: `Fila Super 1`,
-    //       description: `fila`,
-    //       price: 49
-    //     },{ //4
-    //       id : 105,
-    //       roomImg : settings.imagePath+`img05.jpg`,
-    //       starImg: settings.starsPath+`0star.png`,
-    //       title: `Puma Super 1`,
-    //       description: `puma`,
-    //       price: 49
-    //     },{ //5
-    //       id : 106,
-    //       roomImg : settings.imagePath+`img06.jpg`,
-    //       starImg: settings.starsPath+`5stars.png`,
-    //       title: `Champ Super 2`,
-    //       description: `champion`,
-    //       price: 139
-    //     }
-    // ]
+    const allRooms =[
+      { //0
+        id : 101,
+        roomImg : settings.imagePath+`img01.jpg`,
+        starImg: settings.starsPath+`2stars.png`,
+        title: `Hidden Gem of ...`,
+        description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
+        price: 66
+      },{ //1
+        id : 102,
+        roomImg : settings.imagePath+`img02.jpg`,
+        starImg: settings.starsPath+`3stars.png`,
+        title: `Joshua Tree ...`,
+        description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
+        price: 159
+      },{ //2
+        id : 103,
+        roomImg : settings.imagePath+`img03.jpg`,
+        starImg: settings.starsPath+`4stars.png`,
+        title: `1 Bdrm Modern ...`,
+        description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
+        price: 99 
+      },{ //3
+        id : 104,
+        roomImg : settings.imagePath+`img04.jpg`,
+        starImg: settings.starsPath+`1star.png`,
+        title: `Entire Chalet. South Lake ...`,
+        description: `The Lake Tahoe Chalet`,
+        price: 249
+      },{ //4
+        id : 105,
+        roomImg : settings.imagePath+`img05.jpg`,
+        starImg: settings.starsPath+`0star.png`,
+        title: `Entire house ...`,
+        description: `The Lake Tahoe Chalet`,
+        price: 149 
+      },{ //5
+        id : 106,
+        roomImg : settings.imagePath+`img06.jpg`,
+        starImg: settings.starsPath+`5stars.png`,
+        title: `Unique Architecture ...`,
+        description: `Entire home, Sparkling clean, Laskarina is a Superhost, 	
+        Great location`,
+        price: 572
+      }
+    ];
 
     res.render('roomlisting',{
         title: "Room Listing",
         // headingInfo : "Room Listing",
-        // rooms : allRooms
+        rooms : allRooms
 
     });
 });
