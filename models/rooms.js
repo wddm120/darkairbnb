ca7acefd77315380b340a4f239bc72377185c318
Ivandi Santoso <ivandisantoso@Vahns-MacBook-Pro.local>
2020-02-25 01:47:15 -0500

    Working on user reg form

--- models/rooms.js
+++ models/rooms.js
@@ -1,31 +1,9 @@
-// const prouduct =
-// {
 
-//     fakeDB : [],
-
-//     init()
-//     {
-
-//         this.fakeDB.push({title:'XPS 13',description:`Our smallest 13-inch laptops feature a virtually 
-//         borderless InfinityEdge display and up to 10th gen Intel® processors. 
-//         Touch, silver, rose gold and frost options available
-//         `,price:`1349.99`});
-    
-//         this.fakeDB.push({title:'XPS 15',description:`Powerhouse performance with the latest processors and NVIDIA 
-//         graphics paired with a stunning 4K Ultra HD display.`,price:`1749.99`});
-    
-//         this. fakeDB.push({title:'XPS 17',description:`XPS 17 is designed to keep you entertained for more than 9 hours 
-//         with a 9-cell battery upgrade.`,price:`1949.99`});
-
-
-//         this. fakeDB.push({title:'toy',description:`nice toy.everyone loves toys`,price:`149.99`});
-//     },
-//     getallProducts()
-//     {
-//         return this.fakeDB;
-//     }
-
-// }
+const settings = {
+  imagePath: `img/rooms/`,
+  starsPath: `img/stars/`
+  // sliderPath: `img/sliders/`
+}
 
 const room = 
 {
@@ -35,48 +13,48 @@ const room =
   {
     this.fakeDB.push({
         id : 110,
-        // roomImg : settings.imagePath+`img01.jpg`,
-        // starImg: settings.starsPath+`2stars.png`,
+        roomImg : settings.imagePath+`img01.jpg`,
+        starImg: settings.starsPath+`2stars.png`,
         title: `Hidden Gem of ...`,
         description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
         price: 66
     });
     this.fakeDB.push({
         id : 111,
-        // roomImg : settings.imagePath+`img02.jpg`,
-        // starImg: settings.starsPath+`3stars.png`,
+        roomImg : settings.imagePath+`img02.jpg`,
+        starImg: settings.starsPath+`3stars.png`,
         title: `Joshua Tree ...`,
         description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
         price: 159
         });
     this.fakeDB.push({
         id : 112,
-        // roomImg : settings.imagePath+`img03.jpg`,
-        // starImg: settings.starsPath+`4stars.png`,
+        roomImg : settings.imagePath+`img03.jpg`,
+        starImg: settings.starsPath+`4stars.png`,
         title: `1 Bdrm Modern ...`,
         description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
         price: 99 
     });
     this.fakeDB.push({
         id : 113,
-        // roomImg : settings.imagePath+`img04.jpg`,
-        // starImg: settings.starsPath+`1star.png`,
+        roomImg : settings.imagePath+`img04.jpg`,
+        starImg: settings.starsPath+`1star.png`,
         title: `Entire Chalet. South Lake ...`,
         description: `The Lake Tahoe Chalet`,
         price: 249
     });
     this.fakeDB.push({
         id : 114,
-        // roomImg : settings.imagePath+`img05.jpg`,
-        // starImg: settings.starsPath+`0star.png`,
+        roomImg : settings.imagePath+`img05.jpg`,
+        starImg: settings.starsPath+`0star.png`,
         title: `Entire house ...`,
         description: `The Lake Tahoe Chalet`,
         price: 149 
     });
     this.fakeDB.push({
         id : 115,
-        // roomImg : settings.imagePath+`img06.jpg`,
-        // starImg: settings.starsPath+`5stars.png`,
+        roomImg : settings.imagePath+`img06.jpg`,
+        starImg: settings.starsPath+`5stars.png`,
         title: `Hector Cave House`,
         description: `Hector Cave House, carved into the unique caldera cliff for more than 250 years, was originally used as a wine cellar.`,
         price: 572
@@ -88,54 +66,55 @@ const room =
   }
 }
 
-room.init();
-module.exports=room;
-
-// const allRooms =[
+// const promoRooms =[
 //   { //0
-//     id : 110,
-//     roomImg : settings.imagePath+`img01.jpg`,
+//     id : 101,
+//     roomImg : settings.imagePath+`img20.png`,
 //     starImg: settings.starsPath+`2stars.png`,
-//     title: `Hidden Gem of ...`,
-//     description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
+//     title: `Walnut TREEHOUSE`,
+//     description: `You’ll have the treehouse to yourself`,
 //     price: 66
 //   },{ //1
-//     id : 111,
-//     roomImg : settings.imagePath+`img02.jpg`,
+//     id : 102,
+//     roomImg : settings.imagePath+`img21.png`,
 //     starImg: settings.starsPath+`3stars.png`,
-//     title: `Joshua Tree ...`,
-//     description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
+//     title: `Treehouse + Cabin Retreat`,
+//     description: `The Treehouse Retreat + Cabin Retreat ...`,
 //     price: 159
 //   },{ //2
-//     id : 112,
-//     roomImg : settings.imagePath+`img03.jpg`,
+//     id : 103,
+//     roomImg : settings.imagePath+`img22.png`,
 //     starImg: settings.starsPath+`4stars.png`,
-//     title: `1 Bdrm Modern ...`,
-//     description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
+//     title: `River Eye - Treehouse`,
+//     description: `Experience being in the trees, looking ...`,
 //     price: 99 
 //   },{ //3
-//     id : 113,
-//     roomImg : settings.imagePath+`img04.jpg`,
+//     id : 104,
+//     roomImg : settings.imagePath+`img23.png`,
 //     starImg: settings.starsPath+`1star.png`,
-//     title: `Entire Chalet. South Lake ...`,
-//     description: `The Lake Tahoe Chalet`,
+//     title: `THE MAJESTIC TREEHOUSE romantic, luxury retreat ...`,
 //     price: 249
 //   },{ //4
-//     id : 114,
-//     roomImg : settings.imagePath+`img05.jpg`,
+//     id : 105,
+//     roomImg : settings.imagePath+`img24.png`,
 //     starImg: settings.starsPath+`0star.png`,
-//     title: `Entire house ...`,
-//     description: `The Lake Tahoe Chalet`,
+//     title: `Forest Camping Hut`,
+//     description: `Enjoy a private redwood forest camping hut ...`,
 //     price: 149 
 //   },{ //5
-//     id : 115,
-//     roomImg : settings.imagePath+`img06.jpg`,
+//     id : 106,
+//     roomImg : settings.imagePath+`img25.png`,
 //     starImg: settings.starsPath+`5stars.png`,
-//     title: `Hector Cave House`,
-//     description: `Hector Cave House, carved into the unique caldera cliff for more than 250 years, was originally used as a wine cellar.`,
+//     title: `The ReTreet House`,
+//     description: `Be a part of nature and stay at a house among the trees ...`,
 //     price: 572
 //   }
 // ];
+
+room.init();
+module.exports=room;
+
+
 
 
 
--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const productModel = require("./models/rooms");
 
-const settings = {
-    imagePath: `img/rooms/`,
-    starsPath: `img/stars/`
-    // sliderPath: `img/sliders/`
-  }
-
-
 
 //This allows express to make my static content available from the public
 app.use(express.static('public'))
@@ -39,59 +32,13 @@ app.set('view engine', 'hbs');
 
 
 //set up routes
-app.get('/',(req,res)=>{
-
-  const promoRooms =[
-    { //0
-      id : 101,
-      roomImg : settings.imagePath+`img20.png`,
-      starImg: settings.starsPath+`2stars.png`,
-      title: `Walnut TREEHOUSE`,
-      description: `You’ll have the treehouse to yourself`,
-      price: 66
-    },{ //1
-      id : 102,
-      roomImg : settings.imagePath+`img21.png`,
-      starImg: settings.starsPath+`3stars.png`,
-      title: `Treehouse + Cabin Retreat`,
-      description: `The Treehouse Retreat + Cabin Retreat ...`,
-      price: 159
-    },{ //2
-      id : 103,
-      roomImg : settings.imagePath+`img22.png`,
-      starImg: settings.starsPath+`4stars.png`,
-      title: `River Eye - Treehouse`,
-      description: `Experience being in the trees, looking ...`,
-      price: 99 
-    },{ //3
-      id : 104,
-      roomImg : settings.imagePath+`img23.png`,
-      starImg: settings.starsPath+`1star.png`,
-      title: `THE MAJESTIC TREEHOUSE romantic, luxury retreat ...`,
-      price: 249
-    },{ //4
-      id : 105,
-      roomImg : settings.imagePath+`img24.png`,
-      starImg: settings.starsPath+`0star.png`,
-      title: `Forest Camping Hut`,
-      description: `Enjoy a private redwood forest camping hut ...`,
-      price: 149 
-    },{ //5
-      id : 106,
-      roomImg : settings.imagePath+`img25.png`,
-      starImg: settings.starsPath+`5stars.png`,
-      title: `The ReTreet House`,
-      description: `Be a part of nature and stay at a house among the trees ...`,
-      price: 572
-    }
-  ];
-
-    res.render('index',{
-        title: "Home",
-        indexRooms : promoRooms
-        // carousel : sliders
-    });
-    
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: "Home",
+    // indexRooms : promoRooms
+    rooms: productModel.getAllRooms()
+    // carousel : sliders
+  });
 });
 
 // app.get('/login',(req,res)=>{
@@ -107,61 +54,62 @@ app.get('/signup',(req,res)=>{
   });
 });
 
-app.post('/signup',(req,res)=>{
-  const errors= [];
+app.post('/signup', (req, res) => {
+  const errors = [];
+  const userReg = [];
 
-  if(req.body.email=="")
-  {
-    errors.push("Email is required");
+  if (req.body.email == "") {
+    errors.push("Email is required");  
   }
-  if(req.body.phoneNumber=="")
-  {
-    errors.push("Phone number is required");
+  if (req.body.phoneNumber == "") {
+    errors.push("Phone number is required"); 
   }
-  if(req.body.fistName=="")
-  {
+  if (req.body.fistName == "") {
     errors.push("First name is required");
   }
-  if(req.body.password=="")
-  {
+  if (req.body.password == "") {
     errors.push("Password is required");
-    
-  }else if(req.body.password.length<8)
-  {
+  } else if (req.body.password.length < 8) {
     errors.push("use at least 8 characters");
-  }else{
-    errors.push("Okay");
-  }
-  
-  if(errors.length > 0)
-  {
-    res.render('signup',{
-      messages : errors
-    });
-  }else{
-    // errors.push("Okay");
-    {
-      const accountSid = 'PUT YOUR ACCOUNT SID HERE';
-      const authToken = 'PUT HERE YOUR AUTHTOKEN HERE';
-      const client = require('twilio')(accountSid, authToken);
-      
-      client.messages
-        .create({
-           body: `${req.body.firstName} ${req.body.lastName} Message :${req.body.message}`,
-           from: 'PUT YOUR TRIAL NUMBER HERE',
-           to: `${req.body.phoneNumber}`
-         })
-        .then(message => {
-          console.log(message.sid);
-          res.render("index");
-        })
-        .catch((err)=>{
-            console.log(`Error ${err}`);
-        })
-  
-    }
-  }
+  } 
+  else {
+    userReg.push(`${req.body.email}`);
+    userReg.push(`${req.body.phoneNumber}`);
+    userReg.push(`${req.body.fistName}`);
 
+  }
+  if (errors.length > 0) {
+    res.render('signup', {
+      messages: errors
+    });
+  } else {
+    res.render('index', {
+      title: "Home",
+      userRegInfo: userReg,
+      rooms: productModel.getAllRooms()
+    });
+    // {
+    //   const accountSid = 'PUT YOUR ACCOUNT SID HERE';
+    //   const authToken = 'PUT HERE YOUR AUTHTOKEN HERE';
+    //   const client = require('twilio')(accountSid, authToken);
+
+    //   client.messages
+    //     .create({
+    //       body: `${req.body.firstName} ${req.body.lastName} Message :${req.body.message}`,
+    //       from: '+14805088297',
+    //       to: `${req.body.phoneNumber}`
+    //     })
+    //     .then(message => {
+    //       console.log(message.sid);
+    //       res.render("index");
+    //     })
+    //     .catch((err) => {
+    //       console.log(`Error ${err}`);
+    //     })
+    // }
+    
+  }
+ 
 });
 
 
@@ -209,53 +157,6 @@ app.post('/login',(req,res)=>{
 
 app.get('/rooms',(req,res)=>{
 
-
-    // const allRooms =[
-    //   { //0
-    //     id : 110,
-    //     roomImg : settings.imagePath+`img01.jpg`,
-    //     starImg: settings.starsPath+`2stars.png`,
-    //     title: `Hidden Gem of ...`,
-    //     description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
-    //     price: 66
-    //   },{ //1
-    //     id : 111,
-    //     roomImg : settings.imagePath+`img02.jpg`,
-    //     starImg: settings.starsPath+`3stars.png`,
-    //     title: `Joshua Tree ...`,
-    //     description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
-    //     price: 159
-    //   },{ //2
-    //     id : 112,
-    //     roomImg : settings.imagePath+`img03.jpg`,
-    //     starImg: settings.starsPath+`4stars.png`,
-    //     title: `1 Bdrm Modern ...`,
-    //     description: `Entire home, Self check-in, Sparkling clean, Victoria And Christian is a Superhost`,
-    //     price: 99 
-    //   },{ //3
-    //     id : 113,
-    //     roomImg : settings.imagePath+`img04.jpg`,
-    //     starImg: settings.starsPath+`1star.png`,
-    //     title: `Entire Chalet. South Lake ...`,
-    //     description: `The Lake Tahoe Chalet`,
-    //     price: 249
-    //   },{ //4
-    //     id : 114,
-    //     roomImg : settings.imagePath+`img05.jpg`,
-    //     starImg: settings.starsPath+`0star.png`,
-    //     title: `Entire house ...`,
-    //     description: `The Lake Tahoe Chalet`,
-    //     price: 149 
-    //   },{ //5
-    //     id : 115,
-    //     roomImg : settings.imagePath+`img06.jpg`,
-    //     starImg: settings.starsPath+`5stars.png`,
-    //     title: `Hector Cave House`,
-    //     description: `Hector Cave House, carved into the unique caldera cliff for more than 250 years, was originally used as a wine cellar.`,
-    //     price: 572
-    //   }
-    // ];
-
     res.render('roomlisting',{
         title : "Room Listing Page",
         // rooms : allRooms
@@ -266,9 +167,6 @@ app.get('/rooms',(req,res)=>{
 
 
 // app.get("/products",(req,res)=>{
-
-    
-
 
 //   res.render("product",{
 //       title:"Room Listing Page",
