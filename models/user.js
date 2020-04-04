e6055b9f539f166c7c2f91d2e4baaa9767f0d095
Ivandi Santoso <ivandisantoso@Vahns-MacBook-Pro.local>
2020-04-04 15:24:48 -0400

    bcryptjs implemented

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+
 
 
 //this indicates the shapes of the documents that will be entering the database
@@ -56,6 +58,26 @@ const userSchema = new Schema({
   }
 
 });
+
+
+userSchema.pre("save",function(next){
+  //salt random generated characters or strings
+  bcrypt.genSalt(10)
+  .then((salt)=>{
+      bcrypt.hash(this.password,salt)
+      .then((encryptPassword)=>{
+        this.password = encryptPassword;
+        next();
+      })
+      .catch(err=>console.log(`Error happened while hashing: ${err}`));
+
+  })
+
+  .catch(err=>console.log(`Error occured when salting: ${err}`));
+
+
+})
+
 
 // for every schema you create (create a schema per collection) you must also create a model object. The model will allow you to perform CRUD operations on a given collection! 
 const userModel = mongoose.model('User', userSchema);
